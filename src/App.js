502a05c7f06861717bca059f5a8643091c76777c
Floresa Zogaj <floresaa.zogaj@gmail.com>
2024-08-08 00:00:15 +0200

    useEffect

--- src/App.js
+++ src/App.js
@@ -24,22 +24,4 @@ export default function App() {
   );
 }
 
-    
-//   const[windowWidth, setwindowWidth]=useState(window.innerWidth)
-//   const handleResize=()=>{
-//     setwindowWidth(window.innerWidth)
-//   }
-//   useEffect(()=>{
-//     window.addEventListener('resize', handleResize)
-//   return ()=>{
-//     window.removeEventListener('resize', handleResize)
-//   }
-// },
-// [])
-// return(
-//     <div>{windowWidth} 
-
-//     </div>
-//   )
-    
-// }
+ 
